e6bf8609eaf6e1ec02bb81df6c432d4c985795ee
ribeirokkjj <antonio_ribeiro-n@estudante.sesisenai.org.br>
2026-05-06 11:10:33 -0300

    Update script reference and clean up validation logic in login script

--- scripts/validacao-login.js
+++ scripts/validacao-login.js
@@ -5,9 +5,6 @@ e.preventDefault()
 let usuario = document.getElementById("usuario-login").value
 let senha = document.getElementById("senha-login").value
 
-return alert("adasdsdadsads")
-if (usuario.length == 0) return alert("O campo usuário deve estar preenchido")
-if (senha.length == 0) return alert("O campo senha deve estar preenchido")
 if (usuario !== "a") return alert("Usuário ou senha incorretos")
 if (senha !== "1") return alert("Usuário ou senha incorretos")
 return window.location.href = "secao.html"
