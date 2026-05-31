2f574f61db4566a100550c88145a1fae59a94d23
ribeirokkjj <antonio_ribeiro-n@estudante.sesisenai.org.br>
2026-05-31 13:41:41 -0300

    fix - bugs do modal corrigido e editor de funcionário agora pega o nome também

--- scripts/funcionarios.js
+++ scripts/funcionarios.js
@@ -14,8 +14,6 @@ document.getElementById("form-funcionarios").addEventListener("submit", function
 
     let existingIds = Array.from(tbody.querySelectorAll('tr'))
         .map(tr => parseInt(tr.id)); // pega os IDs dos <tr>  
-    //    let nextId = (Math.max(0, ...existingIds) + 1).toString(); //armazena o maior ID disponível
-
     counter++;
     nextId = counter.toString();
 
@@ -52,52 +50,28 @@ document.querySelector('table').addEventListener('click', function (e) {
 
     if (botao.classList.contains('editar')) { //a partir daqui é o editor de funcionário
 
-        tr = botao.closest('tr');
+        let tr = botao.closest('tr');
 
         if (tr) {
             let setor = tr.querySelector('.setor-tr').textContent;
             let nome = tr.querySelector('.nome-tr').textContent;
-            let trHTML = document.createElement('tr');
-            trHTML.innerHTML = `
-            <div class="modal fade" id="modalCadastro" tabindex="-1" aria-labelledby="modalCadastroLabel"
-                aria-hidden="true">
-                <div class="modal-dialog">
-                    <div class="modal-content text-start">
-                        <div class="modal-header">
-                            <h5 class="modal-title fw-bold" id="modalCadastroLabel">Editar Funcionário</h5>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div class="modal-body">
-                            <form id="form-sensor">
-                                <div class="mb-3">
-                                    <label for="id-sensor" class="form-label fw-bold">Nome Usuário</label>
-                                    <input type="text" class="form-control" id="id-sensor" value="${nome}"required>
-                                </div>
-                                <div class="mb-3">
-                                    <label class="fw-bold form-label">Setor</label>
-                                    <select class="form-select" id="edit-setor">
-                                    <option>Gestão</option>
-                                    <option>Chefe - Setor</option>
-                                    <option>Operacional</option>
-                                    <option>Administrativo</option>
-                                    <option>Funcionários</option>
-                                    </select>
-                                </div>
-                            </form>
-                        </div>
-                        <div class="modal-footer justify-content-between">
-                            <button type="button" class="btn btn-secondary fw-bold"
-                                data-bs-dismiss="modal">Cancelar</button>
-                            <button type="button" class="btn btn-laranja" id="btn-salvar">Salvar Sensor</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-    `;
-            document.querySelector('main').appendChild(trHTML);
+
+            document.getElementById('edit-nome').value = nome;
             document.getElementById('edit-setor').value = setor;
         }
         return;
     }
 
 }); //a ideia é que o editor venha com os items dos ids mais proximos do botao de click, e quando eu crico em submit no botao do editor ele muda os td do id com os dados novos
+
+document.getElementById("form-editor").addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    let novoNome = document.getElementById('edit-nome').value.trim();
+    let novoSetor = document.getElementById('edit-setor').value.trim();
+
+    tr.querySelector('.nome-tr').textContent = novoNome;
+    tr.querySelector('.setor-tr').textContent = novoSetor;
+
+    
+})
