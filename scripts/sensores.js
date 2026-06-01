c2fca187f5df334b71b877e23a48df4a6bb58cc3
Ribeiro <antonio_ribeiro-n@estudante.sesisenai.org.br>
2026-06-01 15:06:40 -0300

    feat - progresso no sensores.js

--- scripts/sensores.js
+++ scripts/sensores.js
@@ -4,19 +4,14 @@ let tr = document.createElement('tr');
 // //ADICIONADOR DE FUNCIONÁRIOS
 // document.getElementById("form-funcionarios").addEventListener("submit", function (e) {
 //     e.preventDefault();
-
 //     let nome = document.getElementById("nome").value.trim();//obs: vou apagar essas anotações futuramente, atualmente elas são para estudo
 //     let setor = document.getElementById("setor").value;//obs: o trim faz pegar os valores tirando os espaços laterais acidentais
-
 //     let tbody = document.querySelector('table tbody');
-
 //     let lastID; // guarda globalmente o último ID para não ter duplicatas
-
 //     let existingIds = Array.from(tbody.querySelectorAll('tr'))
 //         .map(tr => parseInt(tr.id)); // pega os IDs dos <tr>  
 //     counter++;
 //     nextId = counter.toString();
-
 //     tr = document.createElement('tr');
 //     tr.id = nextId;
 //     tr.innerHTML = `
@@ -32,7 +27,6 @@ let tr = document.createElement('tr');
 //         </tr>
 //     `; //padStart 5 faz ter cinco caracteres no total, e sempre zero caso não haja outro número para ser colocado
 //     tbody.appendChild(tr); //cria o tr já editado na última linha do 'table tbody'
-
 //     e.target.reset();
 // });
 
@@ -68,17 +62,17 @@ document.querySelector('table').addEventListener('click', function (e) {
 
 }); //a ideia é que o editor venha com os items dos ids mais proximos do botao de click, e quando eu crico em submit no botao do editor ele muda os td do id com os dados novos
 
-// document.getElementById("form-editor").addEventListener("submit", function (e) {
-//     e.preventDefault();
-
-//     let novoNome = document.getElementById('edit-nome').value.trim();
-//     let novoSetor = document.getElementById('edit-setor').value.trim();
-
-//     const modal = bootstrap.Modal.getInstance(document.getElementById('modalCadastro'));
-//     modal.hide();
-
-//     tr.querySelector('.nome-tr').textContent = novoNome;
-//     tr.querySelector('.setor-tr').textContent = novoSetor;
-
-    
-//  })
+ document.getElementById("form-sensor").addEventListener("submit", function (e) {
+    e.preventDefault();
+        let novoNome = document.getElementById('edit-nome').value.trim();
+        let novoResponsavel = document.getElementById('edit-responsavel').value.trim();
+        let novoTipo = document.getElementById('edit-tipo').value.trim();
+        let novoLocalizacao = document.getElementById('edit-localizacao').value.trim();
+    const modal = bootstrap.Modal.getInstance(document.getElementById('modalCadastro'));
+    modal.hide();
+    tr.querySelector('.nome-tr').textContent = novoNome;
+    tr.querySelector('.responsavel-tr').textContent = novoResponsavel;
+    tr.querySelector('.tipo-tr').textContent = novoTipo;
+    tr.querySelector('.localizacao-tr').textContent = novoLocalizacao;
+  
+  })
